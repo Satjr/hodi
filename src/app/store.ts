64f424be052aa830a9bc71fd6f7d44930b3589de
vterzian <valentin.terzian@saflab.io>
2021-08-11 18:32:20 +0200

    feat(HOD-178333352): add possibility to select a card
    review(HOD-178333352): remove some yoda and useless action
    
    review(HOD-178333352): add unSelectCard action

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,13 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 import boardReducer from '../features/board/boardSlice';
+import selectedCardReducer from '../features/card/selectedCardSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     board: boardReducer,
+    selectedCard: selectedCardReducer,
   },
 });
 
--- src/features/board/Board.tsx
+++ src/features/board/Board.tsx
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { board, attack } from './boardSlice';
+import {selectCard, unSelectCard, selectedCard} from '../card/selectedCardSlice';
 import { useAppSelector, useAppDispatch } from '../../app/hooks';
 import { CardInterface, Card } from '../card/Card';
+import { useSelector } from 'react-redux';
 
 export function Board () {
     const playerCards: CardInterface[] = useAppSelector(board).playerSide.cards;
     const opponentCards: CardInterface[] = useAppSelector(board).opponentSide.cards;
     const dispatch = useAppDispatch();
+    const selectedCardPosition = useSelector(selectedCard);
+
+    useEffect(() => {
+        window.addEventListener('mouseup', handleDeselectCard);
+        return () => {
+            window.removeEventListener('mouseup', handleDeselectCard);
+        };
+    }, [selectedCardPosition]);
+
+    const handleSelectCard = (cardPosition:number) => {
+        if (selectedCardPosition === null) {
+            dispatch(selectCard(cardPosition))
+        }
+    }
+
+    const handleDeselectCard = (event:MouseEvent) => {
+        if (event.which === 3) {
+            event.preventDefault();
+            dispatch(unSelectCard());
+        }
+    }
+
+    const handleContextMenu = (event:React.MouseEvent<HTMLInputElement>) => {
+        event.preventDefault();
+    }
 
     return (
         <>
@@ -15,11 +42,23 @@ export function Board () {
             <button onClick={() => dispatch(attack([1, 0]))}> Dragon attack Mage</button>
             <button onClick={() => dispatch(attack([1, 1]))}> Dragon attack Vampire</button>
             <div>
-                { opponentCards.map((card, i) => <Card key={`${card}${i}`} cardProperties={card} />) }
+                { opponentCards.map((card, i) => (
+                    <div>
+                        <Card key={`${card}${i}`} cardProperties={card} />
+                    </div>
+                )) }
             </div>
             <hr/>
             <div>
-                { playerCards.map((card, i) => <Card key={`${card}${i}`} cardProperties={card} />) }
+                { playerCards.map((card, i) => (
+                    <div
+                        onClick={() => { handleSelectCard(i) }}
+                        onContextMenu={handleContextMenu}
+                        style={{border: (selectedCardPosition === i) ? '1px solid black': ''}}
+                    >
+                        <Card key={`${card}${i}`} cardProperties={card} />
+                    </div>
+                )) }
             </div>
         </>
     );
